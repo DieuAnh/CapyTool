97d2a6246a63e149c34b7a4318af5b9d03600a81
Dieu Anh <anhlee2101@gmail.com>
2019-10-23 17:17:14 +0200

    update data type in models

--- capytool/models.py
+++ capytool/models.py
@@ -8,8 +8,8 @@ class Invoice(models.Model):
     idi = models.CharField(max_length=200)
     created_at = models.DateTimeField(default=datetime.now)
     client_name = models.CharField(max_length=200)
-    total_ttc = models.IntegerField(default=0)
-    total_tva = models.IntegerField(default=0)
+    total_ttc = models.FloatField(default=0)
+    total_tva = models.FloatField(default=0)
     email = models.CharField(max_length=200)
 
     def __str__(self):
